e24cb6ab20006084d6289f7e3eaf7f19c2c2e614
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-19 20:34:39 +0530

    Mars Rover Problem

--- mars.js
+++ mars.js
@@ -1,40 +1,5 @@
 const currentState = "00N"
 const command = "RLM"
-// const roverCommand = (currentState, command) => {
-//     let result = [];
-//     let currentCordinate = currentState.split("");
-//     let x = parseInt (currentCordinate[0])
-//     let y = parseInt (currentCordinate[1])
-//     let pole = currentCordinate[2]
-//     result.push(x)
-//     result.push(y)
-//     result.push(pole)
-//     let move = command.split("")
-//     //console.log(move)
-//     for(let i = 0; i < move.length; i++) {
-
-//         if(move[i] === 'L'){
-//             if(result[2]==="N") result[2] = "W"
-//             else if(result[2]==="W") result[2] = "S"
-//             else if(result[2]==="S") result[2] = "E"
-//             else if(result[2]==="E") result[2] = "N"
-//         }
-//         else if(move[i] === 'R'){
-//             if(result[2]==="N") result[2] = "E"
-//             else if(result[2]==="E") result[2] = "S"
-//             else if(result[2]==="S") result[2] = "W"
-//             else if(result[2]==="W") result[2] = "N"
-//         }
-//         else if(move[i] === 'M'){
-//             if(result[2]==="N") result[1] += 1;
-//             else if(result[2]==="E") result[0] += 1
-//             else if(result[2]==="S") result[1] -= 1
-//             else if(result[2]==="W") result[0] -= 1
-//         }    
-//     }
-// return result;
-// }
-
 
 const roverCommand = (currentState, command, grid) => {
     let result = [];
@@ -57,7 +22,7 @@ const moveFunction = (command, result, grid) => {
     
   for(let i = 0; i < move.length; i++) {
 
-    if ((result[0] >= roverLimit[0]) || (result[1] >= roverLimit[1])) {
+    if ((result[0] > roverLimit[0]) || (result[1] > roverLimit[1])) {
         return  "My Plot Limit exceeded, My Current Status : " + result;
     }  
     if(move[i] === 'L'){
@@ -82,4 +47,4 @@ const moveFunction = (command, result, grid) => {
 return result;
 }
 
-console.log(roverCommand("00N","RM"))
+console.log(roverCommand("33E","MMRMMRMRRM",55))
